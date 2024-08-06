89e66ab52305f49dfea138411372c9987aab2aa6
vipin6 <vipinlamba.2706@gmail.com>
2024-08-06 15:56:12 +0530

    Your commit message

--- controller/logic.js
+++ controller/logic.js
@@ -1,44 +1,47 @@
-const User=require('../model/data')
-exports.adduser=async(req,res,next)=>{
-    console.log('req.body',req.body)
-    try{
-      if(!req.body.number){
-        throw new Error('phone number is mandatory')
-      }
-    const name=req.body.name;
-    const email=req.body.email;
-    const phonenumber=req.body.number;
-    
-  
-    const data=await User.create({name:name,email:email,phonenumber:number})
-    res.status(201).json(data)
-    }
-    catch(err){
-      res.status(500).json({
-        error:err
-      })
-    }
-  }
-  exports.getElement=async(req,res,next)=>{
-    try{
-    const users=await User.findAll();
-    res.status(200).json(users)
-    }catch(error){
-        console.log('Get user is falling',JSON.stringify(error))
-        res.status(500).json({error:error})
-    }
-  }
-  exports.getDelete=async(req,res,next)=>{
-    try{
-        if(!req.params.id){
-            console.log('id is missing')
-            res.status(400).json({err:'id is missing'})
-        }
-    const uId = req.params.id;
-    await User.destroy({where:{id:uId}})
-    }catch(err){
-        console.log(err);
-        res.status(500).json(err)
+const User = require('../model/data');
 
+exports.adduser = async (req, res) => {
+    console.log('req.body', req.body);
+    try {
+        if (!req.body.number) {
+            throw new Error('Phone number is mandatory');
+        }
+        
+        const { name, email, number: phonenumber } = req.body;
+
+        const data = await User.create({ name, email, phonenumber });
+
+        res.status(201).json(data);
+    } catch (err) {
+        console.error('Error in adduser:', err); 
+        res.status(500).json({ error: err.message });
     }
-  }
+};
+
+exports.getElement = async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.status(200).json(users);
+    } catch (error) {
+        console.error('Error in getElement:', error); 
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getDelete = async (req, res) => {
+    try {
+        const uId = req.params.id;
+
+        if (!uId) {
+            console.log('ID is missing');
+            return res.status(400).json({ error: 'ID is missing' }); 
+        }
+
+        await User.destroy({ where: { id: uId } });
+
+        res.status(204).send(); 
+    } catch (err) {
+        console.error('Error in getDelete:', err); 
+        res.status(500).json({ error: err.message });
+    }
+};
--- public/index.js
+++ public/index.js
@@ -1,81 +1,73 @@
-function userDetails(event){
-    event.preventDefault()
-    const name= event.target.name.value;
-    const emailid=event.target.email.value;
-    const phone1=event.target.number.value;
+function userDetails(event) {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const emailid = event.target.email.value;
+    const phone1 = event.target.number.value;
     
-    const obj={
+    const obj = {
         name,
         emailid,
         phone1
-    }
-    console.log(obj)
+    };
+    console.log(obj);
    
-    axios.post("http://localhost:4000/user/add-user",obj)
-    .then((response) =>{ 
-       
-        shownewuseronscreen(response)
-        
-       
+    axios.post("http://localhost:4000/api/user/add-user", obj)
+    .then((response) => { 
+        shownewuseronscreen(response.data); // Fixed: response.data to access the user data
     })
-    .catch((err) =>{
-        document.body.innerHTML = document.body.innerHTML + "<h4>something went wrong </h4>"
-        console.log(err)
-    })
-    
-}
-
-    window.addEventListener("DOMContentLoaded", () => {
-        
-         axios.get("http://localhost:4000/user/get-users")
-         .then((response) => {
-            console.log(response)
-            for(var i=0;i<response.length;i++){
-                shownewuseronscreen(response[i]);
-            }
-         })
-         .catch((error) => {
-            console.log(error)
-         })
-    
-    })
-
-
-
-function shownewuseronscreen(user){
-    console.log(user)
-    document.getElementById('emailid').value= '';
-    document.getElementById('username').value= '';
-    document.getElementById('phoneo').value= '';
-    
-  
-    const parentnode=document.getElementById('listofusers')
-    const childnode=   `<li id=${user.id}> ${user.name}, ${user.email}
-                        <button onclick=deleteUser('${user.id}')> Delete user </button>
-                        <button onclick=edituser('${user.email}','${user.name}','${user.phonenumber}',${user.id})> Edit User </button></li>`
-    parentnode.innerHTML= parentnode.innerHTML + childnode
-}
-function edituser(emailid,name,phone1,userid){
-    document.getElementById('emailid').value=emailid
-    document.getElementById('username').value=name
-    document.getElementById('phoneo').value=phone1
-    deleteUser(userid)
-
-}
-function deleteUser(userid){
-    axios.delete(`http://localhost:4000/user/delete-user/${userid}`)
-    .then((response) => {
-        removeuserfromscreen(userid)
-
-    })
-    .catch((err) =>{
+    .catch((err) => {
+        document.body.innerHTML += "<h4>something went wrong </h4>";
         console.log(err);
-        res.sendStatus(500).json(err)
-    })
+    });
+}
 
- }
-function removeuserfromscreen(userid){
-    const parentnode=document.getElementById('listofusers')
-    const childnodeisdeleted=document.getElementById(userid)
-    parentnode.removeChild(childnodeisdeleted)
+window.addEventListener("DOMContentLoaded", () => {
+    axios.get("http://localhost:4000/api/user/get-users")
+    .then((response) => {
+        console.log(response.data); // Fixed: response.data to access the user data
+        for (let i = 0; i < response.data.length; i++) {
+            shownewuseronscreen(response.data[i]); // Fixed: response.data[i]
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+});
+
+function shownewuseronscreen(user) {
+    console.log(user);
+    document.getElementById('emailid').value = '';
+    document.getElementById('username').value = '';
+    document.getElementById('phoneo').value = '';
+    
+    const parentnode = document.getElementById('listofusers');
+    const childnode = `<li id=${user.id}> ${user.name}, ${user.emailid}, ${user.phone1} 
+                        <button onclick="deleteUser('${user.id}')"> Delete user </button>
+                        <button onclick="edituser('${user.emailid}', '${user.name}', '${user.phone1}', ${user.id})"> Edit User </button></li>`;
+    parentnode.innerHTML += childnode; // Simplified concatenation
+}
+
+function edituser(emailid, name, phone1, userid) {
+    document.getElementById('emailid').value = emailid;
+    document.getElementById('username').value = name;
+    document.getElementById('phoneo').value = phone1;
+    deleteUser(userid);
+}
+
+function deleteUser(userid) {
+    axios.delete(`http://localhost:4000/api/user/delete-user/${userid}`)
+    .then((response) => {
+        removeuserfromscreen(userid);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+function removeuserfromscreen(userid) {
+    const parentnode = document.getElementById('listofusers');
+    const childnodeisdeleted = document.getElementById(userid);
+    if (childnodeisdeleted) {
+        parentnode.removeChild(childnodeisdeleted);
+    }
 }
